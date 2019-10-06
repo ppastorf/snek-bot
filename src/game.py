fb6a98cf89afd8c3d634e5eb6b3b79cda124c779
PPastorF <pedropastor999@gmail.com>
2019-10-05 23:36:24 -0300

    Refactor

--- src/game.py
+++ src/game.py
@@ -2,18 +2,14 @@ from tkinter import *
 from time import sleep
 import snake as sn
 
-# Total size of the game screen
 X_SIZE = 400
 Y_SIZE = 400
 
-X_OFFSET = 10
-Y_OFFSET = 10
-
-PLAYABLE_X = X_SIZE - X_OFFSET
-PLAYABLE_Y = Y_SIZE - Y_OFFSET
+BORDER_SIZE = 10
 
 SCORE_SIZE = 20
 PIECE_SIZE = 10
+
 
 START_POS = [300, 200]
 START_POS_X = START_POS[0]
@@ -23,55 +19,75 @@ START_DIR = 'left'
 HUMAN_DELAY = 0.04
 BOT_DELAY = 0.01
 
-BORD_COLOR = "#000000"
+BORDER_COLOR = "#000000"
 
 
 class Game(object):
 
-    def __init__(self):
-        self.root = Tk()
-        self.root.title("Snake")
+    def __init__(
+            self,
+            x_size=X_SIZE,
+            y_size=Y_SIZE,
+            show=True,
+            delay=HUMAN_DELAY,
+            win_title="Snake"):
 
-        self.canvas = Canvas(
-            self.root,
-            width=X_SIZE,
-            height=Y_SIZE + SCORE_SIZE
-        )
-        self.canvas.pack(expand=YES, fill=BOTH)
+        if show:
+            root = Tk()
+            root.title(win_title)
+            canvas = Canvas(
+                root,
+                width=x_size,
+                height=y_size + SCORE_SIZE
+            )
+            canvas.pack(expand=YES, fill=BOTH)
+            root.bind('<Return>', self.keyboard_return)
+        else:
+            root = None
+            canvas = None
 
-        self.snake = sn.Snake()
-        self.food = sn.Food(X_SIZE, Y_SIZE)
+        self.x_size = x_size
+        self.y_size = y_size
+
+        self.playable_x = [BORDER_SIZE, self.x_size - BORDER_SIZE]
+        self.playable_y = [BORDER_SIZE, self.y_size - BORDER_SIZE]
+
+        self.root = root
+        self.canvas = canvas
+
+        self.tick_delay = delay
+
+        self.snake = sn.Snake(self, START_POS_X, START_POS_Y)
+        self.food = sn.Food(self)
 
         self.score = 0
-        self.turns = 0
         self.playtime = 0.0
         self.is_alive = True
+
+    @staticmethod
+    def human_playable():
+        game = Game()
+        game.bind_to_keys()
+
+        return game
 
     def show_score(self):
         self.canvas.create_text(
             X_SIZE / 2,
-            Y_SIZE + OFFSET,
+            Y_SIZE + BORDER_SIZE,
             text='Score: {}'.format(self.score))
 
     def keyboard_left(self, event):
-        if self.snake.in_valid_position:
-            self.turns += 1
-            self.snake.turn('left')
+        self.snake.turn('left')
 
     def keyboard_right(self, event):
-        if self.snake.in_valid_position:
-            self.turns += 1
-            self.snake.turn('right')
+        self.snake.turn('right')
 
     def keyboard_up(self, event):
-        if self.snake.in_valid_position:
-            self.turns += 1
-            self.snake.turn('up')
+        self.snake.turn('up')
 
     def keyboard_down(self, event):
-        if self.snake.in_valid_position:
-            self.turns += 1
-            self.snake.turn('down')
+        self.snake.turn('down')
 
     def keyboard_return(self, event):
         self.game_over()
@@ -85,11 +101,11 @@ class Game(object):
 
     def _new_food(self):
         del self.food
-        food = Food(self.canvas)
+        food = sn.Food(self)
         while (
             any([parts.pos == food.pos for parts in self.snake.body]) or
                 self.snake.pos == food.pos):
-            food = Food(self.canvas)
+            food = sn.Food(self)
 
         return food
 
@@ -98,52 +114,52 @@ class Game(object):
         self.food = self._new_food()
         self.score += 1
 
-    def show_border(self):
+    def show_border(self, color=BORDER_COLOR, size=BORDER_SIZE):
         self.canvas.create_line(
             0,
-            OFFSET - 4,
-            X_SIZE + 4,
-            OFFSET - 4,
-            fill=BORD_COLOR,
-            width=OFFSET
+            size - 4,
+            self.x_size + 4,
+            size - 4,
+            fill=color,
+            width=size
         )
 
         self.canvas.create_line(
-            OFFSET - 4,
-            OFFSET - 4,
-            OFFSET - 4,
-            Y_SIZE,
-            fill=BORD_COLOR,
-            width=OFFSET
+            size - 4,
+            size - 4,
+            size - 4,
+            self.y_size,
+            fill=color,
+            width=size
         )
 
         self.canvas.create_line(
-            X_SIZE - 4,
-            OFFSET - 4,
-            X_SIZE - 4,
-            Y_SIZE,
-            fill=BORD_COLOR,
-            width=OFFSET
+            self.x_size - 4,
+            size - 4,
+            self.x_size - 4,
+            self.y_size,
+            fill=color,
+            width=size
         )
 
         self.canvas.create_line(
-            OFFSET - 4,
-            Y_SIZE - 4,
-            X_SIZE - 4,
-            Y_SIZE - 4,
-            fill=BORD_COLOR,
-            width=OFFSET
+            size - 4,
+            self.y_size - 4,
+            self.x_size - 4,
+            self.y_size - 4,
+            fill=color,
+            width=size
         )
 
-    def _draw_screen(self):
+    def draw_screen(self):
         self.root.update_idletasks()
         self.root.update()
 
-    def _clear_screen(self):
+    def clear_screen(self):
         self.canvas.delete("all")
 
-    def _tick(self):
-        self._clear_screen()
+    def tick(self):
+        self.clear_screen()
         self.show_border()
         self.show_score()
         self.food.show()
@@ -156,7 +172,7 @@ class Game(object):
         if self.snake_in_food_position:
             self.snake_eats_food()
 
-        self._draw_screen()
+        self.draw_screen()
         self.snake.walk()
 
     def end(self):
@@ -165,129 +181,110 @@ class Game(object):
         self.canvas.destroy()
         self.root.destroy()
 
-    def _bind_keys(self):
+    def bind_to_keys(self):
         self.root.bind('<Left>', self.keyboard_left)
         self.root.bind('<Right>', self.keyboard_right)
         self.root.bind('<Up>', self.keyboard_up)
         self.root.bind('<Down>', self.keyboard_down)
-        self.root.bind('<Return>', self.keyboard_return)
 
     def play(self):
-        self._bind_keys()
-
-        while self.is_alive:
-            self._tick()
-            self.playtime += HUMAN_DELAY
-            sleep(HUMAN_DELAY)
-
-        self.end()
-
-
-class BotGame(HumanGame):
-    def __init__(self, bot, gen):
-        super().__init__()
-        self.root.title("Snek Bot generation " + str(gen))
-        self.bot = bot
-
-    @property
-    def game_state(self):
-        # distance to wall to the front, left and right
-        dist_left = (self.snake.pos_x - OFFSET) / REAL_X_SIZE
-        dist_up = (self.snake.pos_y - OFFSET) / REAL_Y_SIZE
-        dist_right = 1 - dist_left
-        dist_down = 1 - dist_up
-
-        if self.snake.direction == 'left':
-            walldist_front = dist_left
-            walldist_left = dist_down
-            walldist_right = dist_up
-
-        if self.snake.direction == 'right':
-            walldist_front = dist_right
-            walldist_left = dist_up
-            walldist_right = dist_down
-
-        if self.snake.direction == 'up':
-            walldist_front = dist_up
-            walldist_left = dist_left
-            walldist_right = dist_right
-
-        if self.snake.direction == 'down':
-            walldist_front = dist_down
-            walldist_left = dist_right
-            walldist_right = dist_left
-
-        # angle to food (normalized)
-        food_angle_rad = atan2(self.snake.pos_y - self.food.pos_y,
-                               self.snake.pos_x - self.food.pos_x)
-        food_angle = degrees(food_angle_rad) / 180
-
-        return [walldist_front, walldist_left, walldist_right, food_angle]
-
-    def control_snake(self, action):
-        if action == 'straight':
-            pass
-        else:
-            if self.snake.direction == 'left':
-                if action == 'left':
-                    self.keyboard_down(None)
-                if action == 'right':
-                    self.keyboard_up(None)
-
-            elif self.snake.direction == 'right':
-                if action == 'left':
-                    self.keyboard_up(None)
-                if action == 'right':
-                    self.keyboard_down(None)
-
-            elif self.snake.direction == 'up':
-                if action == 'left':
-                    self.keyboard_left(None)
-                if action == 'right':
-                    self.keyboard_right(None)
-
-            elif self.snake.direction == 'down':
-                if action == 'left':
-                    self.keyboard_right(None)
-                if action == 'right':
-                    self.keyboard_left(None)
-
-    def tick(self):
-        self.canvas.delete("all")
-
-        self.show_border()
-        self.food.show()
-        self.snake.show()
-        self.show_score()
-
-        if not self.snake.in_valid_position:
-            self.game_over()
-            return
-
-        if self.snake_has_eaten_food:
-            self.score_up()
-
-        self.bot.learn_state(self.game_state)
-        action = self.bot.take_action()
-
-        self.control_snake(action)
-
-        self.root.update_idletasks()
-        self.root.update()
-
-        self.snake.walk()
-
-    def play(self):
-        self._bind_keys()
-
         while self.is_alive:
             self.tick()
-            self.playtime += BOT_DELAY
-            sleep(BOT_DELAY)
+            self.snake.time_alive += self.tick_delay
+            sleep(self.tick_delay)
 
         self.end()
 
 
 if __name__ == '__main__':
-    game = HumanGame()
+    game = Game.human_playable()
     game.play()
+
+
+#     @property
+#     def game_state(self):
+#         # distance to wall to the front, left and right
+#         dist_left = (self.snake.pos_x - BORDER_SIZE) / REAL_X_SIZE
+#         dist_up = (self.snake.pos_y - BORDER_SIZE) / REAL_Y_SIZE
+#         dist_right = 1 - dist_left
+#         dist_down = 1 - dist_up
+
+#         if self.snake.direction == 'left':
+#             walldist_front = dist_left
+#             walldist_left = dist_down
+#             walldist_right = dist_up
+
+#         if self.snake.direction == 'right':
+#             walldist_front = dist_right
+#             walldist_left = dist_up
+#             walldist_right = dist_down
+
+#         if self.snake.direction == 'up':
+#             walldist_front = dist_up
+#             walldist_left = dist_left
+#             walldist_right = dist_right
+
+#         if self.snake.direction == 'down':
+#             walldist_front = dist_down
+#             walldist_left = dist_right
+#             walldist_right = dist_left
+
+#         # angle to food (normalized)
+#         food_angle_rad = atan2(self.snake.pos_y - self.food.pos_y,
+#                                self.snake.pos_x - self.food.pos_x)
+#         food_angle = degrees(food_angle_rad) / 180
+
+#         return [walldist_front, walldist_left, walldist_right, food_angle]
+
+#     def control_snake(self, action):
+#         if action == 'straight':
+#             pass
+#         else:
+#             if self.snake.direction == 'left':
+#                 if action == 'left':
+#                     self.keyboard_down(None)
+#                 if action == 'right':
+#                     self.keyboard_up(None)
+
+#             elif self.snake.direction == 'right':
+#                 if action == 'left':
+#                     self.keyboard_up(None)
+#                 if action == 'right':
+#                     self.keyboard_down(None)
+
+#             elif self.snake.direction == 'up':
+#                 if action == 'left':
+#                     self.keyboard_left(None)
+#                 if action == 'right':
+#                     self.keyboard_right(None)
+
+#             elif self.snake.direction == 'down':
+#                 if action == 'left':
+#                     self.keyboard_right(None)
+#                 if action == 'right':
+#                     self.keyboard_left(None)
+
+#     def tick(self):
+#         self.canvas.delete("all")
+
+#         self.show_border()
+#         self.food.show()
+#         self.snake.show()
+#         self.show_score()
+
+#         if not self.snake.in_valid_position:
+#             self.game_over()
+#             return
+
+#         if self.snake_has_eaten_food:
+#             self.score_up()
+
+#         self.bot.learn_state(self.game_state)
+#         action = self.bot.take_action()
+
+#         self.control_snake(action)
+
+#         self.root.update_idletasks()
+#         self.root.update()
+
+#         self.snake.walk()
--- src/snake.py
+++ src/snake.py
@@ -1,9 +1,5 @@
 from random import randrange
 
-X_SIZE = 400
-Y_SIZE = 400
-OFFSET = 10
-
 DEFAULT_SIZE = 10
 DEFAULT_COLOR = "#aaaaaa"
 
@@ -16,16 +12,21 @@ BODY_COLOR = "#e68a00"
 
 
 class Element(object):
-    def __init__(self, pos_x, pos_y,
-                 color=DEFAULT_COLOR,
-                 size=DEFAULT_SIZE):
+    def __init__(
+            self,
+            game,
+            pos_x, pos_y,
+            color=DEFAULT_COLOR,
+            size=DEFAULT_SIZE):
+
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.size = size
+        self.game = game
 
-    def show(self, canvas):
-        canvas.create_rectangle(
+    def show(self):
+        self.game.canvas.create_rectangle(
             self.pos_x,
             self.pos_y,
             self.pos_x + self.size,
@@ -41,19 +42,27 @@ class Element(object):
 class Food(Element):
     def __init__(
             self,
-            mapsize_x, mapsize_y,
+            game,
             size=FOOD_SIZE, color=FOOD_COLOR):
         super().__init__(
-            randrange(0, mapsize_x, size),
-            randrange(0, mapsize_y, size),
+            game,
+            randrange(game.playable_x[0], game.playable_x[1], size),
+            randrange(game.playable_y[0], game.playable_y[1], size),
             color=color
         )
 
 
 class SnakeBody(Element):
 
-    def __init__(self, last_part, size=SNAKE_SIZE, color=BODY_COLOR):
+    def __init__(
+            self,
+            master,
+            last_part,
+            size=SNAKE_SIZE,
+            color=BODY_COLOR):
+
         super().__init__(
+            master.game,
             last_part.last_pos_x,
             last_part.last_pos_y,
             color=color
@@ -78,8 +87,16 @@ class SnakeBody(Element):
 
 class SnakeHead(Element):
 
-    def __init__(self, start_x, start_y, size=SNAKE_SIZE, color=HEAD_COLOR):
+    def __init__(
+            self,
+            master,
+            start_x,
+            start_y,
+            size=SNAKE_SIZE,
+            color=HEAD_COLOR):
+
         super().__init__(
+            master.game,
             start_x,
             start_y,
             color=color
@@ -106,23 +123,33 @@ class SnakeHead(Element):
 
 
 class Snake(object):
-    def __init__(self, start_dir='left', lenght=1, size=SNAKE_SIZE):
+    def __init__(
+            self,
+            game,
+            start_x,
+            start_y,
+            start_dir='left',
+            lenght=1, size=SNAKE_SIZE):
+
+        self.game = game
         self.direction = start_dir
-        self.head = SnakeHead()
+        self.head = SnakeHead(self, start_x, start_y)
         self.body = []
+        self.turns = 0
+        self.time_alive = 0.0
 
         for i in range(lenght):
             self.body.append(
-                SnakeBody(self.head)
+                SnakeBody(self, self.head)
             )
 
-    def show_body(self, canvas):
+    def show_body(self):
         for i in range(0, self.body_size):
-            self.body[i].show(canvas)
+            self.body[i].show()
 
-    def show(self, canvas):
-        self.head.show(canvas)
-        self.show_body(canvas)
+    def show(self):
+        self.head.show()
+        self.show_body()
 
     def walk(self):
         self.head.walk(self.direction)
@@ -131,6 +158,11 @@ class Snake(object):
             self.body[i].walk()
 
     def turn(self, new_dir):
+        if not self.in_valid_position:
+            return
+
+        self.turns += 1
+
         if new_dir == 'left':
             if self.direction != 'right':
                 self.direction = 'left'
@@ -147,31 +179,35 @@ class Snake(object):
             if self.direction != 'up':
                 self.direction = 'down'
 
+    def eat(self):
+        self.body.append(
+            SnakeBody(self, self.body[self.body_size - 1])
+        )
+
     @property
-    def pos_x(self):
-        return self.head.pos_x
+    def in_valid_position(self):
+        min_x = self.game.playable_x[0]
+        max_x = self.game.playable_x[1]
+        min_y = self.game.playable_y[0]
+        max_y = self.game.playable_y[1]
+
+        return not (
+            (self.pos_x >= max_x or self.pos_x < min_x or
+             self.pos_y >= max_y or self.pos_y < min_y) or
+            any([parts.pos == self.pos for parts in self.body])
+        )
 
     @property
     def body_size(self):
         return len(self.body)
 
     @property
-    def in_valid_position(self):
-        return not (
-            (self.pos_x >= X_SIZE - OFFSET or self.pos_x < OFFSET or
-             self.pos_y >= Y_SIZE - OFFSET or self.pos_y < OFFSET) or
-            (any([parts.pos == self.pos for parts in self.body]))
-        )
-
-    @property
-    def eat(self):
-        self.body.append(
-            SnakeBody(self.body[self.body_size - 1])
-        )
-
-    @property
     def pos_y(self):
         return self.head.pos_y
+
+    @property
+    def pos_x(self):
+        return self.head.pos_x
 
     @property
     def pos(self):
